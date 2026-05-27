b8ec8c3931cecdbb5cba301d06938b8c07f5918e
azuratime2025 <maksum_aj@yahoo.co.id>
2026-05-26 21:59:44 -0400

    chore: bump version to 3.7.4 and test Firebase CI/CD pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.azuratech.azuratime"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3703
-        versionName = "3.7.3"
+        versionCode = 3704
+        versionName = "3.7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GEMINI_API_KEY", "\"$geminiApiKey\"")
