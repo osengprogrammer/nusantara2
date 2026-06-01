a90761b2351f54432dca556b14d9f03aea571fb8
azuratime2025 <maksum_aj@yahoo.co.id>
2026-06-01 07:13:55 -0400

    chore(release): bump to Added new features and bug fixes (3713)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.azuratech.azuratime"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3712
-        versionName = "Implement 100% AI-native Barcode Scanner for Student Attendance"
+        versionCode = 3713
+        versionName = "Added new features and bug fixes"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GEMINI_API_KEY", "\"$geminiApiKey\"")
