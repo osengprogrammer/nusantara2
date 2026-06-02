1d92dbdf3addb80043d19972fa58ac7f7452003b
azuratime2025 <maksum_aj@yahoo.co.id>
2026-06-02 06:30:52 -0400

    chore(release): bump to 3.14.0 (3724)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.azuratech.azuratime"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3723
+        versionCode = 3724
         versionName = "3.14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
